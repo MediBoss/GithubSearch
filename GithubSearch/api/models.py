9f2235abdf8d20a8965ddffbd5a6a8c9db5dcd18
Medi Assumani <mediassumani49@gmail.com>
2019-06-25 19:31:18 -0700

    [Add] request api and User model

--- GithubSearch/api/models.py
+++ GithubSearch/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db import models
 # Create your models here.
+
+class User(models.Model):
+
+    login = models.CharField(max_length=20)
+    avatar_url = models.CharField()
+    url = models.CharField()
--- GithubSearch/api/views.py
+++ GithubSearch/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("We up and runninnnn")
+    
+    req = requests.get("https://api.github.com/search/users?q=MediBoss")
+    parsed_data = json.loads(req.text)
+    result_array = parsed_data["items"]
+    return HttpResponse(result_array)
+
+
