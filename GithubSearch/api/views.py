db74b71467df8c88fb99eb49cdfb1bfbe86a395a
Medi Assumani <mediassumani49@gmail.com>
2019-07-04 01:48:33 -0700

    help plis

--- GithubSearch/api/views.py
+++ GithubSearch/api/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 import requests
 import json
 from django.shortcuts import render
+from api.models import User
 # Create your views here.
-
-
 
 def home(request):
 
@@ -19,6 +18,6 @@ def search(request):
     parsed_data = json.loads(req.text)
     result_array = parsed_data["items"]
 
-    return HttpResponse(result_array)
+    return render(request, 'api/result.html', {'results': result_array})
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GithubSearch.wsgi
